b6db76e1b2b2f674545b799077bccadd9e6a1427
Rodrigo Vecco Haddad <rodrigo@novoingenios.com>
2025-11-09 13:26:56 -0500

    Update loading placeholder icon and submodule references

--- src/mwap/modules/cus/templates/html/loading/placeholder.php
+++ src/mwap/modules/cus/templates/html/loading/placeholder.php
@@ -11,7 +11,7 @@ class mwcus_cus_templates_html_loading_placeholder extends mwmod_mw_bootstrap_ht
 		$this->set_main_elem($main);
 		$iconcontainer=new mwmod_mw_bootstrap_html_def("mw_loading_placeholder_in","div");
 		//$iconcontainer->add_cont("<div class='glyphicon glyphicon-refresh glyphicon-refresh-animate '>");
-		$iconcontainer->add_cont("<div class='glyphicon glyphicon-refresh glyphicon-refresh-animate '></div>");
+		$iconcontainer->add_cont("<div class='fa fa-sync fa-spin '></div>");
 		$main->add_cont($iconcontainer);
 		//$main->add_cont("<div class='glyphicon glyphicon-refresh glyphicon-refresh-animate '>");
 		
