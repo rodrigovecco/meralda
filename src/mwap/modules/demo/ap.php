a1500b9c37be7797b63958f0703e887e8baa16ac
Rodrigo Vecco Haddad <rodrigo@novoingenios.com>
2025-11-22 14:21:36 -0500

    Update .gitignore, remove composer files, and add demo submanager in mwap_demo_ap class

--- src/mwap/modules/demo/ap.php
+++ src/mwap/modules/demo/ap.php
@@ -7,6 +7,10 @@ class mwap_demo_ap  extends mwmod_mw_ap_def{
 		$man=new mwap_demo_uiadmin_main($this);
 		return $man;	
 	}
+	function create_submanager_demo(){
+		$man=new mwmod_mw_demo_man_man($this);
+		return $man;	
+	}
 }
 
 ?>
